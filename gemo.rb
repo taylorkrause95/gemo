06100eeb25b7a8a94be3fd4d5267ea40bf2c6360
taylorkrause95 <taylor@brainstormtech.io>
2018-09-20 11:21:15 -0400

    Initial commit with countries gem

---
+++ gemo.rb
@@ -0,0 +1,14 @@
+require 'countries'
+
+puts "What country do you want a report on?"
+
+# strip removes white space
+country_name = gets.strip
+
+country = ISO3166::Country.find_country_by_name(country_name)
+
+if country
+  puts "The flag for #{country.name} is #{country.emoji_flag}"
+else
+  puts "Could not find a country called #{country_name}"
+end
